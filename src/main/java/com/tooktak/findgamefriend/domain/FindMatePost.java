e0ddf3c1772085800986a5a24a19def59fe6b6a8
boseok <asdww6631@gmail.com>
2022-06-11 19:17:02 +0900

    FindMatePost 추가

---
+++ src/main/java/com/tooktak/findgamefriend/domain/FindMatePost.java
@@ -0,0 +1,11 @@
+package com.tooktak.findgamefriend.domain;
+
+public class FindMatePost {
+
+    private Long id;
+    private String postId;
+    private String contents;
+    private String hashTag;
+    private String kakaoLink;
+    private String discordLink;
+}
