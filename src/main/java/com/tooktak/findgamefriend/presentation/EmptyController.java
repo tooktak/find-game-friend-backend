a6d4fc712e711c2ecba99e1d427c47949e4f53c9
dojunho <djh20@kumoh.ac.kr>
2022-06-11 20:06:44 +0900

    remove lombok

--- src/main/java/com/tooktak/findgamefriend/presentation/EmptyController.java
+++ src/main/java/com/tooktak/findgamefriend/presentation/EmptyController.java
@@ -2,16 +2,20 @@ package com.tooktak.findgamefriend.presentation;
 
 import com.tooktak.findgamefriend.domain.User;
 import com.tooktak.findgamefriend.service.UserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/public")
-@RequiredArgsConstructor
 public class EmptyController {
     private final UserService userService;
+
+    @Autowired
+    public EmptyController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/home")
     public String home() {
--- src/main/java/com/tooktak/findgamefriend/service/UserService.java
+++ src/main/java/com/tooktak/findgamefriend/service/UserService.java
@@ -2,16 +2,20 @@ package com.tooktak.findgamefriend.service;
 
 import com.tooktak.findgamefriend.domain.User;
 import com.tooktak.findgamefriend.infrastructure.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 @Transactional
-@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User save() {
         User user = new User("dasdsdas", "asdasdsadasdk", "emial@emial", "nickname", "url");
