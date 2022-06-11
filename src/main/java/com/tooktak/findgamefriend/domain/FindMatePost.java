8918e08797ef55463e8ac4f991ea04626b40ceb2
dojunho <djh20@kumoh.ac.kr>
2022-06-12 01:34:02 +0900

    add latest pull up date

--- src/main/java/com/tooktak/findgamefriend/domain/FindMatePost.java
+++ src/main/java/com/tooktak/findgamefriend/domain/FindMatePost.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 public class FindMatePost extends BaseEntity {
@@ -28,12 +29,16 @@ public class FindMatePost extends BaseEntity {
     @Column(columnDefinition = "TEXT")
     private String discordLink;
 
-    public FindMatePost(String title, String contents, String hashTag, String kakaoLink, String discordLink) {
+    @Column
+    private LocalDateTime latestPullUpDateTime;
+
+    public FindMatePost(String title, String contents, String hashTag, String kakaoLink, String discordLink, LocalDateTime latestPullUpDateTime) {
         this.title = title;
         this.contents = contents;
         this.hashTag = hashTag;
         this.kakaoLink = kakaoLink;
         this.discordLink = discordLink;
+        this.latestPullUpDateTime = latestPullUpDateTime;
     }
 
     public FindMatePost() {
--- src/main/java/com/tooktak/findgamefriend/service/MemberService.java
+++ src/main/java/com/tooktak/findgamefriend/service/MemberService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 
 @Service
 public class MemberService {
@@ -20,7 +22,14 @@ public class MemberService {
 
     @Transactional
     public Member register(MemberRegisterRequest req) {
-        Member member = new Member(req.getMemberId(), req.getPassword(), req.getEmail(), req.getNickName(), req.getPictureURL(), MemberType.NORMAL);
+        Member member = new Member(
+                req.getMemberId(),
+                req.getPassword(),
+                req.getEmail(),
+                req.getNickName(),
+                req.getPictureURL(),
+                MemberType.NORMAL
+        );
         return memberRepository.save(member);
     }
 }
