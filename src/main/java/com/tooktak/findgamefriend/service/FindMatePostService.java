2f1013f51b35270d4a41b10a2f890ff4d5ac2dbe
dojunho <djh20@kumoh.ac.kr>
2022-06-12 03:03:34 +0900

    remove redundant test

--- src/main/java/com/tooktak/findgamefriend/service/FindMatePostService.java
+++ src/main/java/com/tooktak/findgamefriend/service/FindMatePostService.java
@@ -1,6 +1,7 @@
 package com.tooktak.findgamefriend.service;
 
 import com.tooktak.findgamefriend.domain.FindMatePost;
+import com.tooktak.findgamefriend.domain.Member;
 import com.tooktak.findgamefriend.infrastructure.FindMatePostRepository;
 import com.tooktak.findgamefriend.service.dto.FindMatePost.FindMatePostRegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,6 @@ public class FindMatePostService {
     }
 
     public FindMatePost save(FindMatePostRegisterRequest req) {
-        FindMatePost findMatePost = new FindMatePost(req.getTitle(), req.getContents(),req.getHashtag(),req.getKakaoLink(),req.getDiscordLink());
-        return findMatePostRepository.save(findMatePost);
+        return null;
     }
 }
