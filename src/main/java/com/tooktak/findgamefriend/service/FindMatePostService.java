b8a9b2839f6060a9e1375e733bd4b59d50fec9f5
boseok <asdww6631@gmail.com>
2022-06-11 19:26:23 +0900

    FindMatePost 추가

---
+++ src/main/java/com/tooktak/findgamefriend/service/FindMatePostService.java
@@ -0,0 +1,13 @@
+package com.tooktak.findgamefriend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FindMatePostService {
+    private final
+}
