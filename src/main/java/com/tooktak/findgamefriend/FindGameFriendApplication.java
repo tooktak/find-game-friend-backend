f1104f5f1ded33bcd7bcc9df2b6d331c89cd8d87
dojunho <djh20@kumoh.ac.kr>
2022-06-11 20:52:05 +0900

    add jpa audit

--- src/main/java/com/tooktak/findgamefriend/FindGameFriendApplication.java
+++ src/main/java/com/tooktak/findgamefriend/FindGameFriendApplication.java
@@ -2,11 +2,18 @@ package com.tooktak.findgamefriend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableWebSecurity
 public class FindGameFriendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC")); // use UTC
 		SpringApplication.run(FindGameFriendApplication.class, args);
 	}
 }
