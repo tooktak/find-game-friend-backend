e90695ce17fbb71bdfc74f1e09db6363eb3c792e
dojunho <djh20@kumoh.ac.kr>
2022-06-20 22:35:41 +0900

    test

--- src/main/java/com/tooktak/findgamefriend/FindGameFriendApplication.java
+++ src/main/java/com/tooktak/findgamefriend/FindGameFriendApplication.java
@@ -15,6 +15,5 @@ public class FindGameFriendApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC")); // use UTC
 		SpringApplication.run(FindGameFriendApplication.class, args);
-		dasdsada
 	}
 }
