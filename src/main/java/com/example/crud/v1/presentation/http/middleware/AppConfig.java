2df8772df522b3b935852187ee15e6092937e517
boseok <asdww6631@gmail.com>
2023-05-24 00:42:28 +0900

    Cors 403 설정

--- src/main/java/com/example/crud/v1/presentation/http/middleware/AppConfig.java
+++ src/main/java/com/example/crud/v1/presentation/http/middleware/AppConfig.java
@@ -17,7 +17,7 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://aribomy.com")
+                .allowedOrigins("https://aribomy.com")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3600);
